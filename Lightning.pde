cd7df611eaa5ab89ac8813238359259307d932c4
shleewhite <lwhite@sportsbasement.com>
2014-09-04 10:02:06 -0700

    changed lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,31 @@
+int startX = 150;
+int startY = 150;
+int endX = 0;
+int endY = 0;
 void setup()
 {
-  size(300,300);
+	size(600, 600);
+	strokeWeight(1);
+	background(0, 0, 0);
 }
 void draw()
 {
-
+	stroke(0, (int)(Math.random()*256), (int)(Math.random()*256));
+	while (endX < 600) 
+	{
+		
+		endX = startX + (int)((Math.random()*10) - 5);
+		endY = startY + (int)((Math.random()*10) - 5);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+	}
 }
 void mousePressed()
 {
-
+	startX = 150;
+	startY = 150;
+	endX = 0;
+	endY = 0;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,35 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 150;
+int startY = 150;
+int endX = 0;
+int endY = 0;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	size(600, 600);
+	strokeWeight(1);
+	background(0, 0, 0);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	stroke(0, (int)(Math.random()*256), (int)(Math.random()*256));
+	while (endX < 600) 
+	{
+		
+		endX = startX + (int)((Math.random()*10) - 5);
+		endY = startY + (int)((Math.random()*10) - 5);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+	}
 }
 public void mousePressed()
 {
-  redraw();
+	startX = 150;
+	startY = 150;
+	endX = 0;
+	endY = 0;
 }
 
   static public void main(String[] passedArgs) {
